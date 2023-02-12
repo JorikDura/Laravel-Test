a8ec27f06bf2cd7bd09d2d02d3a83d61bc90ea23
chymishhh <vandarkslave@outlook.com>
2023-02-13 02:07:18 +0300

    dependencies

---
+++ webpack.mix.js
@@ -0,0 +1,15 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css',
+        []);
+
+//подрубаем jQuery
+mix.autoload({
+    jquery: ['$', 'window.$', 'window.jQuery']
+});
+
+mix
+    .css('resources/css/auth_styles.css', 'public/css')
+    .js('resources/js/create_list.js', 'public/js')
+    .js('resources/js/bootstrap-inputs.js', 'public/js')
